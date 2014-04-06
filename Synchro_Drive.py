183cb836ed15725072d42295087a5049675ded42
Wayne C. Gramlich <Wayne@Gramlich.Net>
2014-04-06 15:50:15 +0000

    Added visible/invisible to Part.

--- Synchro_Drive.py
+++ Synchro_Drive.py
@@ -216,6 +216,7 @@ class Motor_Assembly(Part):
 	drive_motor_pulley_top = motor_assembly.drive_motor_pulley_top_
 	#drive_motor_pulley_top = motor_assembly.drive_motor_pulley_top_
 	motor_base = motor_assembly.motor_base_
+	motor_base_cover = motor_assembly.motor_base_cover_
 	pcb = motor_assembly.pcb_
 	synchro_drive = self.up
 	twist_magnet = motor_assembly.twist_magnet_
@@ -227,6 +228,8 @@ class Motor_Assembly(Part):
 	# Define some constants:
 	zero = L()
 	sqrt2 = math.sqrt(2)
+
+	motor_base_cover.invisible_set()
 
 	# Grab some values from *wheel_assembly:
 	wheel_assembly = synchro_drive.wheel_assembly_
